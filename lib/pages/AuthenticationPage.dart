dd4e32f57dbca3d92a78195e2c5619486156a75d
ArunKuleri <arunk.aru1@gmail.com>
2024-02-02 13:21:09 +0530

    created a otp screen

--- lib/pages/AuthenticationPage.dart
+++ lib/pages/AuthenticationPage.dart
@@ -1,10 +1,149 @@
+import 'package:country_picker/country_picker.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widget/custom_button.dart';
 
-class AuthenticationPage extends StatelessWidget {
+class AuthenticationPage extends StatefulWidget {
   const AuthenticationPage({super.key});
 
   @override
+  State<AuthenticationPage> createState() => _AuthenticationPageState();
+}
+
+class _AuthenticationPageState extends State<AuthenticationPage> {
+  final TextEditingController phoneController = TextEditingController();
+  Country country = Country(
+      phoneCode: "91",
+      countryCode: "IN",
+      e164Sc: 0,
+      geographic: true,
+      level: 1,
+      name: "India",
+      example: "India",
+      displayName: "India",
+      displayNameNoCountryCode: "IN",
+      e164Key: "IN");
+  @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    phoneController.selection = TextSelection.fromPosition(
+        TextPosition(offset: phoneController.text.length));
+    return Scaffold(
+      backgroundColor: Colors.orangeAccent,
+      body: SafeArea(
+          child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 35),
+          child: Column(
+            children: [
+              Container(
+                width: 250,
+                height: 250,
+                child: Image.asset("assets/images/canon1.png"),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Register",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text(
+                "Add your Phone number. We'll send your a verification code",
+                style: TextStyle(
+                  fontSize: 14,
+                  color: Colors.white,
+                  fontWeight: FontWeight.normal,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              TextFormField(
+                  cursorColor: Colors.orangeAccent,
+                  controller: phoneController,
+                  style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold),
+                  onChanged: (value) {
+                    setState(() {
+                      phoneController.text = value;
+                    });
+                  },
+                  decoration: InputDecoration(
+                    hintText: "Enter Phone Number",
+                    hintStyle: TextStyle(
+                        fontWeight: FontWeight.w500,
+                        fontSize: 15,
+                        color: Colors.white),
+                    hoverColor: Colors.white,
+                    enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    focusedBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10),
+                      borderSide: const BorderSide(color: Colors.white),
+                    ),
+                    prefixIcon: Container(
+                      padding: EdgeInsets.all(8.0),
+                      child: InkWell(
+                        onTap: () {
+                          showCountryPicker(
+                              context: context,
+                              countryListTheme:
+                                  CountryListThemeData(bottomSheetHeight: 550),
+                              onSelect: (value) {
+                                setState(() {
+                                  country = value;
+                                });
+                              });
+                        },
+                        child: Text(
+                          "${country.flagEmoji} + ${country.phoneCode}",
+                          style: TextStyle(
+                              fontSize: 18,
+                              color: Colors.white,
+                              fontWeight: FontWeight.bold),
+                        ),
+                      ),
+                    ),
+                    suffixIcon: phoneController.text.length > 9
+                        ? Container(
+                            height: 30,
+                            width: 30,
+                            margin: const EdgeInsets.all(10.0),
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: Colors.green,
+                            ),
+                            child: Icon(
+                              Icons.done,
+                              color: Colors.white,
+                              size: 20,
+                            ),
+                          )
+                        : null,
+                  )),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                width: double.infinity,
+                height: 50,
+                child: CustomButton(text: "Get OTP", onPresseed: () {}),
+              )
+            ],
+          ),
+        ),
+      )),
+    );
   }
 }
---
+++ lib/widget/custom_button.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class CustomButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPresseed;
+  const CustomButton({super.key, required this.text, required this.onPresseed});
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: onPresseed,
+        style: ButtonStyle(
+            foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+            backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+            shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(25.0)))),
+        child: Text(
+          text,
+          style: TextStyle(fontSize: 22),
+        ));
+  }
+}
